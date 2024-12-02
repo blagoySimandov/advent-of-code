1927f50865ac47ea091c86e1f45a13f040ca9c86
blagoySimandov <baggy.simandoff@gmail.com>
2024-12-02 17:55:29 +0000

    pmuhahah2
    Signed-off-by: blagoySimandov <baggy.simandoff@gmail.com>

--- cmd/day2/day2.go
+++ cmd/day2/day2.go
@@ -21,16 +21,15 @@ func main() {
 	}
 	counter := 0
 	for _, v := range inputMatrix {
-		if j := isReportSafe(v); j {
+		if j := isReportSafeWithDamp(v); j {
 			counter++
 		}
 	}
 	spew.Dump(counter)
-	// input := []int{1, 2, 7, 8, 9}
-	//
+	// input := []int{8, 6, 4, 4, 1}
 	// input2 := []int{1, 3, 2, 4, 5}
-	// spew.Dump(isReportSafe(input))
-	// spew.Dump(isReportSafe(input2))
+	// spew.Dump(isReportSafeWithDamp(input))
+	// spew.Dump(isReportSafeWithDamp(input2))
 }
 
 func isReportSafe(arr []int) bool {
@@ -61,6 +60,76 @@ func isReportSafe(arr []int) bool {
 		}
 
 	}
+	return true
+}
+
+func triggerFalse(dampIsHit *bool) bool {
+	if dampIsHit == nil {
+		panic("bro give me something non-nil")
+	}
+	if *dampIsHit {
+		return false
+	}
+	*dampIsHit = true
+	return true
+}
+
+func deleteElement(slice []int, index int) []int {
+	return append(slice[:index], slice[index+1:]...)
+}
+
+func mydeleteElement(slice []int, index int) []int {
+	return append(slice[:index], slice[index+1:]...)
+}
+
+func isReportSafeWithDamp(arr []int) bool {
+	state := UNKNOWN
+	for i := 0; i < len(arr)-1; i++ {
+		current := arr[i]
+		next := arr[i+1]
+		if current == next {
+			arrWithoutNext := deleteElement(arr, i+1)
+			arrWithoutCurrent := deleteElement(arr, i)
+			if isReportSafe(arrWithoutCurrent) || isReportSafe(arrWithoutNext) {
+				return true
+			}
+			return false
+		}
+		var cstate Monotonicity
+		n := current - next
+		if n < 0 {
+			cstate = Decreasing
+		} else {
+			cstate = Increasing
+		}
+
+		if state == UNKNOWN {
+			state = cstate
+		}
+		if state != cstate {
+			arrWithoutNext := deleteElement(arr, i+1)
+			arrWithoutCurrent := deleteElement(arr, i)
+			if isReportSafe(arrWithoutCurrent) && isReportSafe(arrWithoutNext) {
+				return true
+			}
+			return false
+		}
+		absN := numutil.Abs(n)
+		if absN < 1 || absN > 3 {
+			arrWithoutNext := mydeleteElement(arr, i+1)
+			arrWithoutCurrent := mydeleteElement(arr, i)
+			spew.Dump(i + 1)
+			spew.Dump(i)
+			spew.Dump(arr)
+			spew.Dump(arrWithoutCurrent)
+			spew.Dump(arrWithoutNext)
+			if isReportSafe(arrWithoutCurrent) || isReportSafe(arrWithoutNext) {
+				return true
+			}
+			return false
+		}
+	}
+
 	return true
 }
 
